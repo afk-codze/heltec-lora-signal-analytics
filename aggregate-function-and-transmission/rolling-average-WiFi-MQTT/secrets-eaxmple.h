ab9dd2742503420189c570859a89e943a93d0e74
afk-codze <massimiliano.vitale1999@gmail.com>
2025-04-02 20:23:40 +0200

    Add example secrets file for WiFi and Adafruit IO credentials

---
+++ aggregate-function-and-transmission/rolling-average-WiFi-MQTT/secrets-eaxmple.h
@@ -0,0 +1,6 @@
+#include <pgmspace.h>
+
+const char *WIFI_SSID           = "YOUR_SSID";
+const char *WIFI_PASS           = "YOUR_PASSWORD";
+static const char *IO_USERNAME  = "YOUR_USERNAME";
+static const char *IO_KEY       = "YOUR_KEY";
